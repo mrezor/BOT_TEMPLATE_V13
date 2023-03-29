f39f25e0d7e0fbc84c42da2d4580eede3b215d77
Mrezor <mrezorfr@hotmail.com>
2023-03-29 22:36:06 +0200

    BOT_TEMPLATE_V13

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    clients: {
+        token: 'TON TOKEN',
+        name: 'Bot Template V13',
+        logo: '',
+        activity: 'Bot Template V13',
+        embedColor: '#0099ff'
+    }
+}
